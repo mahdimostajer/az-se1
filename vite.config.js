9e65892acea9753bb0dc5f8116014c8a1fa86aeb
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2024-03-05 22:22:46 +0330

    fix: change path in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/az-se1/",
   build:{
     outDir: "build"
   },
