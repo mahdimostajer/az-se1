ccc444ca47b4995f8fc0be86124b843a14613e28
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2024-03-05 22:30:01 +0330

    feat: add base url

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 export default function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Navigate to="/login" replace />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<Signup />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
