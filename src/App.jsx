cd71ce5beb33d91d8668617a5c0785927baaa58f
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2024-03-05 22:33:05 +0330

    feat: fix base url

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Signup from "./pages/Signup";
 
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={"/az-se1/"}>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
