7c6573c8b8053941087f4665c60c8334490f1e42
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2024-03-08 15:30:18 +0330

    style: add better style for login and register

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import MainPage from "./pages/MainPage";
+import { Layout } from "antd";
 
 export default function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </HashRouter>
+    <Layout style={{ minHeight: "100vh" }} >
+      <Layout.Content>
+        <HashRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </HashRouter>
+      </Layout.Content>
+    </Layout >
+
   );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,22 +5,16 @@ import { useNavigate } from "react-router-dom";
 const MainPage = () => {
     const navigate = useNavigate();
     return (
-        <Layout style={{ minHeight: "100vh" }} >
-            <Layout.Content>
-                <Card className="w-1/3 text-center m-auto mt-7">
-                    <h2 className="m-2 text-lg">Welcome</h2>
-                    <h2 className="m-2 text-lg">
-                        To Software Engineering Lab
-                    </h2>
-                    <h4 className="m-2">az1</h4>
-                    <Button className="m-2" onClick={() => navigate("/login")}>Login</Button>
-                    <Button onClick={() => navigate("/signup")}>Signup</Button>
-                </Card>
 
-            </Layout.Content>
-
-        </Layout >
-
+        <Card className="w-1/3 text-center m-auto mt-7">
+            <h2 className="m-2 text-lg">Welcome</h2>
+            <h2 className="m-2 text-lg">
+                To Software Engineering Lab
+            </h2>
+            <h4 className="m-2">az1</h4>
+            <Button className="m-2" onClick={() => navigate("/login")}>Login</Button>
+            <Button onClick={() => navigate("/signup")}>Signup</Button>
+        </Card>
     )
 }
 
