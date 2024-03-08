7eb89e59491744d569a6185dd5f8ec620c5f05ed
darmond157 <bardiarezaeikalantari@gmail.com>
2024-03-08 11:20:41 +0330

    style: change username input to blue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Login() {
 	return (
 		<>
-			<input placeholder="username:" />
+			<input style={{ color: "blue" }} placeholder="username:" />
 			<input placeholder="password:" />
 			<button>Login</button>
 			<Link to="/signup">Create an account</Link>
 		</>
 	);
 }
-
-
