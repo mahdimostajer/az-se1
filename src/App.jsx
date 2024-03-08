392852d95dd19bc85a4a253cbfd725a3e2ccad92
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2024-03-08 15:38:02 +0330

    feat: implement dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import MainPage from "./pages/MainPage";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<MainPage />} />
       </Routes>
     </HashRouter>
